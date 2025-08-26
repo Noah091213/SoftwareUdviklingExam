b3c7144c2a91a3682797fe2932c0a69bfc6cb470
Noah <noahvryens@gmail.com>
2025-08-26 19:01:49 +0200

    Mostly finished the hero class

---
+++ Hero.cpp
@@ -0,0 +1,57 @@
+#include "Hero.hpp"
+#include <iostream>
+
+
+
+Hero::Hero(std::string name) {
+    heroName     = name;
+    maxHP        = 10;
+    currentHP    = maxHP;
+    currentXP    = 0;
+    currentLevel = 1;
+    strength     = 2;
+}
+
+bool Hero::checkLevelUp(){
+    if (currentXP >= currentLevel*1000 ) {  // Check if the current xp is more than the current level times 1000. So every level needs more and more xp
+        currentXP -= currentLevel*1000; // Remove all XP needed to level up. For level 3 fx, 3000 xp is needed to level up, and so that will be removed
+        currentLevel++;     // Increase the level by one
+        strength++;         // Increase strength by one per level
+        maxHP += 2;         // Increase the max HP of the player
+        currentHP = maxHP;  // Reset the heros hp to max due to levelup
+        return true;        // Return that a level up was completed
+    } else {return false;}  // If it is not time to level up yet, return false
+}
+
+int Hero::getCurrentHP() {
+    return currentHP;
+}
+
+int Hero::getStrength() {
+    return strength;
+}
+
+int Hero::getCurrentXP() {
+    return currentXP;
+}
+
+int Hero::getCurrentLevel() {
+    return currentLevel;
+}
+        
+void Hero::setCurrentHP(int newHP){
+    currentHP = newHP;
+}
+void Hero::setCurrentXP(int newXP){
+    currentXP = newXP;
+}
+
+void Hero::takeDamage(int damage) {
+    currentHP -= damage;
+    std::cout << heroName << " took " << damage << " damage, and is now at " << currentHP << " health out of " << maxHP << std::endl;
+}
+
+void Hero::giveXP(int XP) {
+    currentXP += XP;
+    std::cout << heroName << " earned " << XP << " xp, and is currently at " << currentXP << ". " << currentLevel*1000-currentXP << " is needed to level up!" << std::endl;
+}
--- Hero.hpp
+++ Hero.hpp
@@ -5,10 +5,25 @@
 class Hero {
 
     public:
+        Hero(std::string name); //  Constructor, always requires a name, to differentiate heroes
 
+        bool checkLevelUp();
+
+        int getCurrentHP();
+        int getStrength();
+        int getCurrentXP();
+        int getCurrentLevel();
+        
+        void setCurrentHP(int newHP);
+        void setCurrentXP(int newXP);
+
+        void takeDamage(int damage);
+        void giveXP(int XP);
+    
     private:
         
         std::string  heroName;
+        int maxHP;
         int currentHP;
         int currentXP;
         int currentLevel;
